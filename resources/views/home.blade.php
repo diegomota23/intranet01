f1bd4910e85e21567607943a68bcc75c78e2abdb
diego <diegoaxl23@yahoo.com>
2018-02-15 17:03:34 -0300

    fim do dia 15/12/2018

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,11 +11,28 @@
             <div class="panel panel-default">
                 <div class="container-meio">
                     <div class="corpo">
+                        <div class="fundo-meio"></div>
+
+<!--                        conteudo da lateral esquerda-->
                         <div class="content-esquerdo">
-                            <a href="#"><img src="{{ asset('img/home/') }}"></a> 
+                            <a href="#"><img class="slogan" src="{{ asset('varial/img/home/slogan.png') }}"></a> 
+                            <a href="#"><img class="moca" src="{{ asset('varial/img/home/moca.png') }}"></a> 
+                            <ul class="nav nav-pills nav-stacked" role="tablist">
+                                <li class="active"><a href="#">Home</a></li>
+                                <li><a href="#">HTML</a></li>
+                                <li><a href="#">CSS</a></li>
+                                <li><a href="#">About</a></li>        
+                            </ul>
                         </div>
+<!--fim do conteudo da lateral esquerda-->
                         <div class="content-direito">
-                            <div class="menu-navegacao"></div>
+                            <div class="menu-navegacao">
+                                <ul class="nav nav-tabs" role="tablist">
+                                    <li><a href="#">TML</a></li>
+                                    <li><a href="#">CSS</a></li>
+                                    <li><a href="#">About</a></li>        
+                                </ul>
+                            </div>
                             <div class="content-meio">
                                 <div class="meio"></div>
                                 <div class="menu-direito"></div>
