e25a21bbf85fc8967167028b497fa6e4bdf5b283
diego <diegoaxl23@yahoo.com>
2018-02-16 17:05:46 -0300

    fim do dia 16/02/2018

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,7 @@
                     <div class="corpo">
                         <div class="fundo-meio"></div>
 
-<!--                        conteudo da lateral esquerda-->
+                        <!--conteudo da lateral esquerda-->
                         <div class="content-esquerdo">
                             <a href="#"><img class="slogan" src="{{ asset('varial/img/home/slogan.png') }}"></a> 
                             <a href="#"><img class="moca" src="{{ asset('varial/img/home/moca.png') }}"></a> 
@@ -24,18 +24,37 @@
                                 <li><a href="#">About</a></li>        
                             </ul>
                         </div>
-<!--fim do conteudo da lateral esquerda-->
+                        <!--fim do conteudo da lateral esquerda-->
+                        <!--conteudo do menu nav-->
                         <div class="content-direito">
                             <div class="menu-navegacao">
                                 <ul class="nav nav-tabs" role="tablist">
-                                    <li><a href="#">TML</a></li>
+                                    <li class="pri"><a href="#">TML</a></li>
                                     <li><a href="#">CSS</a></li>
                                     <li><a href="#">About</a></li>        
                                 </ul>
                             </div>
+                            <!--fim do conteudo menu  nav-->
                             <div class="content-meio">
                                 <div class="meio"></div>
-                                <div class="menu-direito"></div>
+                                <!--  conteudo do menu direito-->
+                                <div class="menu-direito">
+                                    <table class="t-funcoes">
+                                        <tr>
+                                            <th class="suporte"></th>
+                                            <th class="patrimonio"></th>
+                                            <th class="almoxarifado"></th>
+                                        </tr>
+                                        <tr>
+                                            <th class="lista-tel"></th>
+                                            <th class="email"></th>
+                                        </tr>
+                                    </table>
+                                    <div class="calendario">
+                                        <!-- HTML --> 
+                                     
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,17 +3,33 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!--        <meta name="viewport" content="width=device-width, initial-scale=1">-->
 
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>Intranet</title>
-        
-        @yield('links')
+
+
 
         <!-- Styles -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+       <link href = "https://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css"
+         rel = "stylesheet">
+      <script src = "https://code.jquery.com/jquery-1.10.2.js"></script>
+      <script src = "https://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+            <script>
+      $(function() {
+            $( "#datepicker-2" ).datepicker({
+               appendText:"(yy-mm-dd)",
+               dateFormat:"yy-mm-dd",
+               altField: "#datepicker-4",
+               altFormat: "DD, d MM, yy"
+            });
+         });
+        </script>
+        @yield('links')
+
     </head>
     <body>
         <div id="app">
@@ -22,61 +38,61 @@
                     <div class="navbar-header">
 
                         <!-- Collapsed Hamburger -->
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                            <span class="sr-only">Alternar de navegação</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collaps            e">
+            <span class="sr-only">Alternar de navegação</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
 
-                        <!-- Branding Image -->
-                        <a class="navbar-brand" href="{{ url('/') }}">Intranet
-                            <!--                        {{ config('app.name', 'Laravel') }}-->
+        <!-- Branding Image -->
+        <a class="navbar-brand" href="{{ url('/') }}">Intranet
+            <!--                        {{ config('app.name', 'Laravel') }}-->
 
+        </a>
+    </div>
+
+    <div class="collapse navbar-collapse" id="app-navbar-collapse">
+        <!-- Left Side Of Navbar -->
+        <ul class="nav navbar-nav">
+            &nbsp;
+        </ul>
+
+        <!-- Right Side Of Navbar -->
+        <ul class="nav navbar-nav navbar-right">
+            <!-- Authentication Links -->
+            @if (Auth::guest())
+            <li><a href="{{ route('login') }}">Entrar </a></li>
+            <li><a href="{{ route('register') }}">Registrar</a></li>
+            @else
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                    {{ Auth::user()->name }} <span class="caret"></span>
+                </a>
+
+                <ul class="dropdown-menu" role="menu">
+                    <li>
+                        <a href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                               document.getElementById('logout-form').submit();">
+                            Logout
                         </a>
-                    </div>
 
-                    <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                        <!-- Left Side Of Navbar -->
-                        <ul class="nav navbar-nav">
-                            &nbsp;
-                        </ul>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            {{ csrf_field() }}
+                        </form>
+                    </li>
+                </ul>
+            </li>
+            @endif
+        </ul>
+    </div>
+</div>
+</nav>
 
-                        <!-- Right Side Of Navbar -->
-                        <ul class="nav navbar-nav navbar-right">
-                            <!-- Authentication Links -->
-                            @if (Auth::guest())
-                            <li><a href="{{ route('login') }}">Entrar </a></li>
-                            <li><a href="{{ route('register') }}">Registrar</a></li>
-                            @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                           onclick="event.preventDefault();
-                                                    document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                            @endif
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-     
-        </div>
-            @yield('content')
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}"></script>
-    </body>
+</div>
+@yield('content')
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}"></script>
+</body>
 </html>
