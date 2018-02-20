3ad4c930d92d381cb8233bd20d2876404555f1fd
diego <diegoaxl23@yahoo.com>
2018-02-20 12:50:22 -0300

    configuração do calendario

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,61 @@
 
 @section('links')
 <link href="{{ asset('varial/css/home.css') }}" rel="stylesheet">
+<link href="{{ asset('varial/css/jquery-ui.css') }}" rel="stylesheet">
+<script type="text/javascript" src="<?php echo asset('varial/jquery-ui.js') ?>"></script>
+
+<script>
+$( function() {
+    $( "#datepicker" ).datepicker();
+  } );
+  
+  /* Brazilian initialisation for the jQuery UI date picker plugin. */
+/* Written by Leonildo Costa Silva (leocsilva@gmail.com). */
+( function( factory ) {
+	if ( typeof define === "function" && define.amd ) {
+
+		// AMD. Register as an anonymous module.
+		define( [ "../widgets/datepicker" ], factory );
+	} else {
+
+		// Browser globals
+		factory( jQuery.datepicker );
+	}
+}( function( datepicker ) {
+
+datepicker.regional[ "pt-BR" ] = {
+	closeText: "Fechar",
+	prevText: "&#x3C;Anterior",
+	nextText: "Próximo&#x3E;",
+	currentText: "Hoje",
+	monthNames: [ "Janeiro","Fevereiro","Março","Abril","Maio","Junho",
+	"Julho","Agosto","Setembro","Outubro","Novembro","Dezembro" ],
+	monthNamesShort: [ "Jan","Fev","Mar","Abr","Mai","Jun",
+	"Jul","Ago","Set","Out","Nov","Dez" ],
+	dayNames: [
+		"Domingo",
+		"Segunda-feira",
+		"Terça-feira",
+		"Quarta-feira",
+		"Quinta-feira",
+		"Sexta-feira",
+		"Sábado"
+	],
+	dayNamesShort: [ "Dom","Seg","Ter","Qua","Qui","Sex","Sáb" ],
+	dayNamesMin: [ "Dom","Seg","Ter","Qua","Qui","Sex","Sáb" ],
+	weekHeader: "Sm",
+	dateFormat: "dd/mm/yy",
+	firstDay: 0,
+	isRTL: false,
+	showMonthAfterYear: false,
+	yearSuffix: "" };
+datepicker.setDefaults( datepicker.regional[ "pt-BR" ] );
+
+return datepicker.regional[ "pt-BR" ];
+
+} ) );
+</script>
+
 @endsection
 
 @section('content')
@@ -50,10 +105,7 @@
                                             <th class="email"></th>
                                         </tr>
                                     </table>
-                                    <div class="calendario">
-                                        <!-- HTML --> 
-                                     
-                                    </div>
+                                        Date: <div id="datepicker"></div>
                                 </div>
                             </div>
                         </div>
